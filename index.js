20073e7c80cdd123422f999bdbb2572ac676ae95
MushroomFX <dergolem26@gmail.com>
2023-01-11 12:38:36 +0100

    + added zero-pad

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ function genOTP(hash) {
     let seed = parseInt(hash,16) * time;
     let a = seed * 15485863;
     let b = ((a * a * a) % 2038074743) / 2038074743;
-    let otp = parseInt(b * 100000000,10)
-    return otp % 999999
+    let otp = parseInt(b * 100000000,10) % 999999;
+    while(otp.length<6){
+        otp = "0" + otp 
+    }
+    return otp
 }
-// genOTP('2424234a234a234a234a');
+// genOTP('2424234a234a234a234a')
